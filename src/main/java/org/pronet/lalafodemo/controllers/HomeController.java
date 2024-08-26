c88e5fb0ed732203affa577d12287ac34338a19d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-26 23:38:18 +0400

    Create controller: HomeController

---
+++ src/main/java/org/pronet/lalafodemo/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package org.pronet.lalafodemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping(value = "/")
+    public String homeView() {
+        return "home/home";
+    }
+}
