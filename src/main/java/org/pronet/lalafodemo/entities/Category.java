f7af3a993478b530a0c40e5bddf97c65d846c9b2
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-26 22:58:08 +0400

    Add constructor and getter/setter methods

--- src/main/java/org/pronet/lalafodemo/entities/Category.java
+++ src/main/java/org/pronet/lalafodemo/entities/Category.java
@@ -17,4 +17,46 @@ public class Category {
     private String imageName;
     @CreationTimestamp
     private LocalDateTime createdDateTime;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String imageName, LocalDateTime createdDateTime) {
+        this.id = id;
+        this.name = name;
+        this.imageName = imageName;
+        this.createdDateTime = createdDateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
 }
