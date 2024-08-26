042fb03ec1ddd59fee1b9322d7d2da55647f7d6c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-27 01:15:38 +0400

    Add new method: findAllByNameContainingIgnoreCase()

--- src/main/java/org/pronet/lalafodemo/repositories/CategoryRepository.java
+++ src/main/java/org/pronet/lalafodemo/repositories/CategoryRepository.java
@@ -4,7 +4,10 @@ import org.pronet.lalafodemo.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByNameContainingIgnoreCase(String name);
     Boolean existsByNameContainingIgnoreCase(String name);
 }
