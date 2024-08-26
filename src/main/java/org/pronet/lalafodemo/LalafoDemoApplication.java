01ca34b931c82d3d24f533ddbeea32a13645e377
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-26 22:49:07 +0400

    Create "Lalafo" Demo Web App using Spring Initializr

---
+++ src/main/java/org/pronet/lalafodemo/LalafoDemoApplication.java
@@ -0,0 +1,13 @@
+package org.pronet.lalafodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalafoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LalafoDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pronet/lalafodemo/LalafoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pronet.lalafodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalafoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
