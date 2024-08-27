7ec33917798515c06c4023272d8a3a9b1dddc6fd
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-27 13:13:52 +0400

    Create repository: ProductRepository

---
+++ src/main/java/org/pronet/lalafodemo/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package org.pronet.lalafodemo.repositories;
+
+import org.pronet.lalafodemo.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByNameContainingIgnoreCase(String name);
+}
