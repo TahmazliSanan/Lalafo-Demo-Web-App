c0c9272eeaa6ed9f0187c0df0f5aaa933f7e507e
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-27 13:05:17 +0400

    Modify method body

--- src/main/java/org/pronet/lalafodemo/services/implementations/CategoryServiceImplementation.java
+++ src/main/java/org/pronet/lalafodemo/services/implementations/CategoryServiceImplementation.java
@@ -14,6 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -63,6 +64,7 @@ public class CategoryServiceImplementation implements CategoryService {
         String imageName = file.isEmpty() ? foundedCategory.getImageName() : file.getOriginalFilename();
         foundedCategory.setName(category.getName().trim());
         foundedCategory.setImageName(imageName);
+        foundedCategory.setCreatedDateTime(LocalDateTime.now());
         categoryRepository.save(foundedCategory);
         if (!file.isEmpty()) {
             File savedFile = new ClassPathResource("static/").getFile();
