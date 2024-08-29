816b75a4fcf98b0bfefba90ee91e02c0df6de3b1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-29 14:34:29 +0400

    Create component: CustomUserDetails

---
+++ src/main/java/org/pronet/lalafodemo/security/CustomUserDetails.java
@@ -0,0 +1,33 @@
+package org.pronet.lalafodemo.security;
+
+import org.pronet.lalafodemo.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getName());
+        return List.of(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
