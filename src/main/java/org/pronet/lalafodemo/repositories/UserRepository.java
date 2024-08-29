febb32a5b33c0bcd57f47105d449f7ec95931363
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-29 11:49:00 +0400

    Create repository: UserRepository

---
+++ src/main/java/org/pronet/lalafodemo/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package org.pronet.lalafodemo.repositories;
+
+import org.pronet.lalafodemo.entities.Role;
+import org.pronet.lalafodemo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmailContainingIgnoreCase(String email);
+    Boolean existsByEmailContainingIgnoreCase(String email);
+    Boolean existsByRole(Role role);
+}
