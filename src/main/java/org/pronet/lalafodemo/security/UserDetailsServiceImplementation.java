d2aafbf455938315d08675f1e9be197de904b237
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-29 14:33:50 +0400

    Create service implementation: UserDetailsServiceImplementation

---
+++ src/main/java/org/pronet/lalafodemo/security/UserDetailsServiceImplementation.java
@@ -0,0 +1,22 @@
+package org.pronet.lalafodemo.security;
+
+import org.pronet.lalafodemo.entities.User;
+import org.pronet.lalafodemo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User foundedUser = userRepository.findByEmailContainingIgnoreCase(username.trim());
+        return new CustomUserDetails(foundedUser);
+    }
+}
