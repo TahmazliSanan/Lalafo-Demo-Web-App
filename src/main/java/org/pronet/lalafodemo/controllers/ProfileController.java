8811d17a6da8586b6b52c78496241353585a845c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-31 01:09:31 +0400

    Create controller: ProfileController

---
+++ src/main/java/org/pronet/lalafodemo/controllers/ProfileController.java
@@ -0,0 +1,53 @@
+package org.pronet.lalafodemo.controllers;
+
+import jakarta.servlet.http.HttpSession;
+import org.pronet.lalafodemo.entities.User;
+import org.pronet.lalafodemo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
+
+@Controller
+@RequestMapping(value = "/profile")
+public class ProfileController {
+    @Autowired
+    private UserService userService;
+
+    @ModelAttribute
+    public User getLoggedInUserDetails(
+            Principal principal,
+            Model model) {
+        if (principal != null) {
+            String email = principal.getName();
+            User foundedUser = userService.getUserByEmail(email);
+            model.addAttribute("foundedUser", foundedUser);
+            return foundedUser;
+        } else {
+            return null;
+        }
+    }
+
+    @GetMapping(value = "/view")
+    public String myProfileView(
+            Principal principal,
+            Model model) {
+        User loggedInUser = getLoggedInUserDetails(principal, model);
+        model.addAttribute("loggedInUser", loggedInUser);
+        return "profile/my-profile";
+    }
+
+    @PostMapping(value = "/update")
+    public String updateMyProfile(
+            @ModelAttribute(value = "user") User user,
+            @RequestParam(value = "file") MultipartFile file,
+            HttpSession session) throws IOException {
+        userService.updateUser(user, file);
+        session.setAttribute("successMessage", "Profiliniz uğurla yeniləndi!");
+        return "redirect:/profile/view";
+    }
+}
