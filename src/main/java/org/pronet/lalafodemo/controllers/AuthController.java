3c162b463189b20216649fa50b0f1f3d08993fa1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-01 22:52:54 +0400

    Add new method: changeAccountPassword()

--- src/main/java/org/pronet/lalafodemo/controllers/AuthController.java
+++ src/main/java/org/pronet/lalafodemo/controllers/AuthController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.pronet.lalafodemo.entities.User;
 import org.pronet.lalafodemo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,23 @@ import java.security.Principal;
 public class AuthController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @ModelAttribute
+    public User getLoggedInUserDetails(
+            Principal principal,
+            Model model) {
+        if (principal != null) {
+            String email = principal.getName();
+            User foundedUser = userService.getUserByEmail(email);
+            model.addAttribute("foundedUser", foundedUser);
+            return foundedUser;
+        } else {
+            return null;
+        }
+    }
 
     private User getLoggedInUserDetails(Principal principal) {
         if (principal != null) {
@@ -72,5 +90,31 @@ public class AuthController {
         userService.deleteUserById(loggedInUser.getId());
         session.setAttribute("successMessage", "Hesabınız uğurla silindi!");
         return "redirect:/auth/login-view";
+    }
+
+    @GetMapping(value = "/change-account-password-view")
+    public String changeAccountPasswordView() {
+        return "auth/change-account-password";
+    }
+
+    @PostMapping(value = "/change-account-password")
+    public String changeAccountPassword(
+            @RequestParam(value = "currentPassword") String currentPassword,
+            @RequestParam(value = "newPassword") String newPassword,
+            @RequestParam(value = "confirmPassword") String confirmPassword,
+            Principal principal,
+            HttpSession session) {
+        User loggedInUser = getLoggedInUserDetails(principal);
+        if (passwordEncoder.matches(currentPassword, loggedInUser.getPassword())) {
+            if (newPassword.equals(confirmPassword)) {
+                userService.updatePassword(newPassword, loggedInUser.getId());
+                session.setAttribute("successMessage", "Şifrəniz uğurla dəyişdirildi!");
+            } else {
+                session.setAttribute("errorMessage", "Şifrələr bir-birinə uyğun deyil!");
+            }
+        } else {
+            session.setAttribute("errorMessage", "Daxil etdiyiniz şifrə doğru deyil!");
+        }
+        return "redirect:/auth/change-account-password-view";
     }
 }
