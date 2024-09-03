b977ea8f5a9b9ef241ad9110fab6826dd1489cf8
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-03 18:56:40 +0400

    Create entity: Favorite

---
+++ src/main/java/org/pronet/lalafodemo/entities/Favorite.java
@@ -0,0 +1,47 @@
+package org.pronet.lalafodemo.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Product product;
+
+    public Favorite() {
+    }
+
+    public Favorite(Long id, User user, Product product) {
+        this.id = id;
+        this.user = user;
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
