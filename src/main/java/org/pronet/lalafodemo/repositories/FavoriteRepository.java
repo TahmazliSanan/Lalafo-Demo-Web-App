0d748b0355876e549d53737ec77d939b1f22368a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-03 21:23:02 +0400

    Add new methods: findAllFavoriteListByFilters() and findAllFavoriteListByFiltersUsingStatus()

--- src/main/java/org/pronet/lalafodemo/repositories/FavoriteRepository.java
+++ src/main/java/org/pronet/lalafodemo/repositories/FavoriteRepository.java
@@ -1,13 +1,42 @@
 package org.pronet.lalafodemo.repositories;
 
 import org.pronet.lalafodemo.entities.Favorite;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findAllByUserId(Long userId);
     Favorite findByUserIdAndProductId(Long userId, Long productId);
+
+    @Query("SELECT f FROM Favorite f " +
+            "JOIN f.product p " +
+            "WHERE f.user.id = :userId " +
+            "AND (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))) " +
+            "AND (:minimumPrice IS NULL OR p.price >= :minimumPrice) " +
+            "AND (:maximumPrice IS NULL OR p.price <= :maximumPrice)")
+    Page<Favorite> findAllFavoriteListByFilters(
+            @Param(value = "userId") Long userId,
+            @Param(value = "name") String name,
+            @Param(value = "minimumPrice") Double minimumPrice,
+            @Param(value = "maximumPrice") Double maximumPrice,
+            Pageable pageable);
+
+    @Query("SELECT f FROM Favorite f " +
+            "JOIN f.product p " +
+            "WHERE f.user.id = :userId " +
+            "AND (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))) " +
+            "AND (:minimumPrice IS NULL OR p.price >= :minimumPrice) " +
+            "AND (:maximumPrice IS NULL OR p.price <= :maximumPrice) " +
+            "AND (:status IS NULL OR p.status = :status)")
+    Page<Favorite> findAllFavoriteListByFiltersUsingStatus(
+            @Param(value = "userId") Long userId,
+            @Param(value = "name") String name,
+            @Param(value = "minimumPrice") Double minimumPrice,
+            @Param(value = "maximumPrice") Double maximumPrice,
+            @Param(value = "status") String status,
+            Pageable pageable);
 }
