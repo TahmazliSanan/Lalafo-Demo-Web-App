e1ad0541a4720a4221d315476867cc81945b0edc
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-04 01:43:04 +0400

    Add new method: deleteByUserIdAndProductId()

--- src/main/java/org/pronet/lalafodemo/repositories/FavoriteListRepository.java
+++ src/main/java/org/pronet/lalafodemo/repositories/FavoriteListRepository.java
@@ -1,6 +1,6 @@
 package org.pronet.lalafodemo.repositories;
 
-import org.pronet.lalafodemo.entities.Favorite;
+import org.pronet.lalafodemo.entities.FavoriteList;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,34 +9,36 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    Favorite findByUserIdAndProductId(Long userId, Long productId);
+public interface FavoriteListRepository extends JpaRepository<FavoriteList, Long> {
+    FavoriteList findByUserIdAndProductId(Long userId, Long productId);
 
-    @Query("SELECT f FROM Favorite f " +
+    @Query("SELECT f FROM FavoriteList f " +
             "JOIN f.product p " +
             "WHERE f.user.id = :userId " +
             "AND (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))) " +
             "AND (:minimumPrice IS NULL OR p.price >= :minimumPrice) " +
             "AND (:maximumPrice IS NULL OR p.price <= :maximumPrice)")
-    Page<Favorite> findAllFavoriteListByFilters(
+    Page<FavoriteList> findAllFavoriteListByFilters(
             @Param(value = "userId") Long userId,
             @Param(value = "name") String name,
             @Param(value = "minimumPrice") Double minimumPrice,
             @Param(value = "maximumPrice") Double maximumPrice,
             Pageable pageable);
 
-    @Query("SELECT f FROM Favorite f " +
+    @Query("SELECT f FROM FavoriteList f " +
             "JOIN f.product p " +
             "WHERE f.user.id = :userId " +
             "AND (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))) " +
             "AND (:minimumPrice IS NULL OR p.price >= :minimumPrice) " +
             "AND (:maximumPrice IS NULL OR p.price <= :maximumPrice) " +
             "AND (:status IS NULL OR p.status = :status)")
-    Page<Favorite> findAllFavoriteListByFiltersUsingStatus(
+    Page<FavoriteList> findAllFavoriteListByFiltersUsingStatus(
             @Param(value = "userId") Long userId,
             @Param(value = "name") String name,
             @Param(value = "minimumPrice") Double minimumPrice,
             @Param(value = "maximumPrice") Double maximumPrice,
             @Param(value = "status") String status,
             Pageable pageable);
+
+    void deleteByUserIdAndProductId(Long userId, Long productId);
 }
