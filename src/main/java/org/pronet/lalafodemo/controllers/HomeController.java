064cde8c4f7d901ec8d5f0b8439b0ee340f4897a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-05 12:02:53 +0400

    Modify method

--- src/main/java/org/pronet/lalafodemo/controllers/HomeController.java
+++ src/main/java/org/pronet/lalafodemo/controllers/HomeController.java
@@ -1,6 +1,10 @@
 package org.pronet.lalafodemo.controllers;
 
+import org.pronet.lalafodemo.entities.Category;
+import org.pronet.lalafodemo.entities.Product;
 import org.pronet.lalafodemo.entities.User;
+import org.pronet.lalafodemo.services.CategoryService;
+import org.pronet.lalafodemo.services.ProductService;
 import org.pronet.lalafodemo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,11 +13,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class HomeController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private ProductService productService;
 
     @ModelAttribute
     public void getLoggedInUserDetails(
@@ -29,7 +40,11 @@ public class HomeController {
     }
 
     @GetMapping(value = "/")
-    public String homeView() {
+    public String homeView(Model model) {
+        List<Category> lastAddedFourCategories = categoryService.getLastAddedFourCategories();
+        List<Product> lastAddedFourProducts = productService.getLastAddedFourProducts();
+        model.addAttribute("lastAddedFourCategories", lastAddedFourCategories);
+        model.addAttribute("lastAddedFourProducts", lastAddedFourProducts);
         return "home/home";
     }
 }
