7a5bf2403702188cb60df277903bac36490c8a6f
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-05 12:01:27 +0400

    Add new method: findTop4ByOrderByCreatedDateTimeDesc()

--- src/main/java/org/pronet/lalafodemo/repositories/ProductRepository.java
+++ src/main/java/org/pronet/lalafodemo/repositories/ProductRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findTop4ByOrderByCreatedDateTimeDesc();
     Page<Product> findAllByCategoryId(Long categoryId, Pageable pageable);
     Page<Product> findAllByNameContainingIgnoreCaseAndUserId(String character, Long userId, Pageable pageable);
     Page<Product> findAllByPriceBetweenAndNameContainingIgnoreCase(Double minimumPrice, Double maximumPrice, String character, Pageable pageable);
